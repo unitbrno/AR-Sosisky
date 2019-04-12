fe76c1e62b6ded5fb068c13ed2c14a259a94c0f5
kalalannn <xvorob00@stud.fit.vutbr.cz>
2019-04-12 21:40:43 +0200

    good

--- src/index.py
+++ src/index.py
@@ -1,19 +1,34 @@
 from flask import Flask
 app = Flask(__name__)
 
-with open('./index.html', 'r') as f:
-    l = f.read()
+with open('index.html', 'r') as f:
+    l1 = f.read()
 
+with open('tasks.html', 'r') as f:
+    l2 = f.read()
 
-print(l)
+with open('warnings.html', 'r') as f:
+    l3 = f.read()
+
+with open('both.html', 'r') as f:
+    l4 = f.read()
+
 
 @app.route("/index.html")
 def index():
-    return l
+    return l1
 
 @app.route("/tasks.html")
-def open():
-    pass
+def opes():
+    return l2
+
+@app.route("/warnings.html")
+def oper():
+    return l3
+
+@app.route("/both.html")
+def oden():
+    return l4
 
 
 #@app.route("/warnings.html")
